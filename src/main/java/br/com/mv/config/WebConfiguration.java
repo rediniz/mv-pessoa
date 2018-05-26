690923a4ad07922c86596175f045773826e8fd34
rediniz <redinizf@hotmail.com>
2018-05-26 20:42:32 -0300

    web configuration

---
+++ src/main/java/br/com/mv/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.mv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfiguration {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/");
+		viewResolver.setSuffix(".html");
+		return viewResolver;
+	}
+
+
+}
