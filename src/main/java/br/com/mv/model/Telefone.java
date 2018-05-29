72c9783b8f88c737c9c9af0082ff385afe8bb4bb
Renato Diniz <redinizf@hotmail.com>
2018-05-29 17:46:16 -0300

    remoção de import não utilizado

--- src/main/java/br/com/mv/model/Telefone.java
+++ src/main/java/br/com/mv/model/Telefone.java
@@ -2,13 +2,13 @@ package br.com.mv.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Table(name = "telefone")
+@Table(name = "telefone", uniqueConstraints = {@UniqueConstraint(columnNames = {"ddd", "numero", "pessoa_id"})})
 @Entity
 @Builder
 @AllArgsConstructor
